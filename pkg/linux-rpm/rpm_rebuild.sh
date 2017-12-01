ab65c1713dd31958e413ef050833fc3092a9f290
Will Markley <willmarkley@users.noreply.github.com>
2017-11-30 22:08:42 -0500

    Update rpm_rebuild.sh

--- pkg/linux-rpm/rpm_rebuild.sh
+++ pkg/linux-rpm/rpm_rebuild.sh
@@ -5,7 +5,7 @@ rm -rf /home/wmarkley/rpmbuild/RPMS/x86_64/
 rm -rf /home/wmarkley/rpmbuild/SRPMS/markit-1.0-1.src.rpm
 rm -f /home/wmarkley/rpmbuild/SOURCES/markit-1.0.tar.gz
 cd SOURCES
-wget http://willmarkley.com/markit/markit-1.0.tar.gz
+wget https://github.com/willmarkley/Markit/archive/1.1.tar.gz
 cd ../SPECS
 rpmbuild -ba markit.spec
 rpmlint markit.spec ../RPMS/x86_64/markit-1.0-1.x86_64.rpm ../SRPMS/markit-1.0-1.src.rpm
