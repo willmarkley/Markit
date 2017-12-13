3506fa75401c3e7b22e516aae96c612f0485ff46
Will Markley <willmarkley@users.noreply.github.com>
2017-12-13 01:50:05 -0500

    Update debbuild.sh

--- pkg/deb/debbuild.sh
+++ pkg/deb/debbuild.sh
@@ -5,11 +5,11 @@
 # mkdir ~/markit
 # cd markit
 wget https://github.com/willmarkley/Markit/archive/1.1.tar.gz
-tar -xzvf 1.1.tar.gz
+tar -xzf 1.1.tar.gz
 rm 1.1.tar.gz
 mv Markit-1.1 markit-1.1
 sed -i -e 's/cour.ttf/\/usr\/share\/markit\/cour.ttf/' markit-1.1/src/markit.cpp
-tar -czvf markit-1.1.tar.gz markit-1.1/
+tar -czf markit-1.1.tar.gz markit-1.1/
 
 # Setup debian package format
 cd markit-1.1
