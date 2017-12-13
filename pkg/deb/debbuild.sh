397dcb1137461905e84433b5f803c35114ebf679
Will Markley <willmarkley@users.noreply.github.com>
2017-12-13 00:37:43 -0500

    Update debbuild.sh

--- pkg/deb/debbuild.sh
+++ pkg/deb/debbuild.sh
@@ -5,9 +5,10 @@ sudo apt-get install dh-make libsdl2-dev libsdl2-ttf-dev
 mkdir ~/markit
 cd markit
 wget https://github.com/willmarkley/Markit/archive/1.1.tar.gz
-mv 1.1.tar.gz markit-1.1.tar.gz
-tar -xzvf markit-1.1.tar.gz
+tar -xzvf 1.1.tar.gz
 mv Markit-1.1 markit-1.1
+sed -i -e 's/cour.ttf/\/usr\/share\/markit\/cour.ttf/' markit-1.1/src/markit.cpp
+tar -czvf markit-1.1.tar.gz markit-1.1/
 
 # Setup debian package format
 cd markit-1.1
