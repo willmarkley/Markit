35451f9c352382386f4a4fe243f3cc78742d86eb
Will Markley <willmarkley@users.noreply.github.com>
2017-12-13 01:06:17 -0500

    Update debbuild.sh

--- pkg/deb/debbuild.sh
+++ pkg/deb/debbuild.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Setup build and download source
-sudo apt-get install dh-make libsdl2-dev libsdl2-ttf-dev
-mkdir ~/markit
-cd markit
+# sudo apt-get install dh-make libsdl2-dev libsdl2-ttf-dev
+# mkdir ~/markit
+# cd markit
 wget https://github.com/willmarkley/Markit/archive/1.1.tar.gz
 tar -xzvf 1.1.tar.gz
 rm 1.1.tar.gz
@@ -12,21 +12,12 @@ sed -i -e 's/cour.ttf/\/usr\/share\/markit\/cour.ttf/' markit-1.1/src/markit.cpp
 tar -czvf markit-1.1.tar.gz markit-1.1/
 
 # Setup debian package format
-cd markit-1.1
-dh_make -e willjmarkley@gmail.com -c mit -f ../markit-1.1.tar.gz         # Select 's' as type of package
+cd markit-1.1 && echo -e "s\n" | dh_make -e willjmarkley@gmail.com -c mit -f ../markit-1.1.tar.gz         # Select 's' as type of package
 
 # Modify the debian/ files
-cd debian
-rm *ex *EX README.* control copyright
-cp ../pkg/deb/control .
-cp ../pkg/deb/copyright .
-cd ..
+cd markit-1.1/debian && rm *ex *EX README.* control copyright
+cp markit-1.1/pkg/deb/control markit-1.1/debian
+cp markit-1.1//pkg/deb/copyright markit-1.1/debian
 
 # Build package
-dpkg-buildpackage -us -uc
-
-# Install package
-sudo dpkg -i ../markit_1.1-1_amd64.deb
-
-# Uninstall package
-dpkg --purge markit
+cd markit-1.1 && dpkg-buildpackage -us -uc
